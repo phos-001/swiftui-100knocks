de86337d59b7b92270065c1b94852129ebead103
Yuto Hayashi <130149511+phos-001@users.noreply.github.com>
2023-05-10 14:29:00 +0900

    Lesson4 (#2)
    * Delete ContentView
    
    * Create Lesson4

---
+++ SwiftUI100Knocks/Lesson4.swift
@@ -0,0 +1,34 @@
+//
+//  Lesson4.swift
+//  SwiftUI100Knocks
+//
+//  Created by Yuto Hayashi on 2023/05/10.
+//
+
+import SwiftUI
+
+struct Lesson4: View {
+    
+    let imageURL = URL(string: "https://cdn2.thecatapi.com/images/lisDai2Pf.jpg")
+    
+    var body: some View {
+        AsyncImage(url: imageURL) { image in
+            image.resizable()
+        } placeholder: {
+            ProgressView()
+        }
+        .scaledToFill()
+        .frame(width: 150, height: 150)
+        .cornerRadius(75)
+        .overlay(
+            RoundedRectangle(cornerRadius:75)
+                .stroke(Color.black, lineWidth: 5)
+        )
+    }
+}
+
+struct Lesson4_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson4()
+    }
+}
