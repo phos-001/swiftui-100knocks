dc92fee0302e7afa044efea43b89df8a2ad6faa4
Yuto Hayashi <130149511+phos-001@users.noreply.github.com>
2023-05-10 13:41:06 +0900

    Create ProjectFile (#1)

---
+++ SwiftUI100Knocks/SwiftUI100KnocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100KnocksApp.swift
+//  SwiftUI100Knocks
+//
+//  Created by Yuto Hayashi on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100KnocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
