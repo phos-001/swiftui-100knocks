39c4083e6bbb5171949bdb9ef42245680b9a48ea
YutoHayashi <yuto.hayashi@fuller.co.jp>
2023-12-12 15:46:33 +0900

    Create Lesson67

---
+++ SwiftUI100Knocks/Lesson61~70/Lesson67.swift
@@ -0,0 +1,33 @@
+//
+//  Lesson67.swift
+//  SwiftUI100Knocks
+//
+//  Created by Yuto Hayashi on 2023/12/12.
+//
+
+import SwiftUI
+
+private struct Pokemon: Identifiable {
+    var id: Int
+    let name: String
+}
+
+struct Lesson67: View {
+    let pokemons: [String] = ["Bulbasaur", "Charmander", "Squirtle"]
+    var body: some View {
+        List(pokemons, id: \.self) {  pokemon in
+            HStack {
+                Text(pokemon)
+                Spacer()
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                print(pokemon)
+            }
+        }
+    }
+}
+
+#Preview {
+    Lesson67()
+}
